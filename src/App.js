73749c26d0f7cdfee1a60e58917cec368379a2e4
Stefan Bettaglio <sbettaglio@gmail.com>
2020-03-24 10:01:27 -0400

    explorer complete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import React, { Component } from 'react'
-import HelloWorld from './components/HelloWorld'
 
-const App = () => {
-  return <HelloWorld />
+class App extends Component {
+  state = {
+    feet: 0,
+    inches: 0,
+  }
+
+  inchTracker = (e) => {
+    this.setState({
+      inches: e.target.value,
+      feet: e.target.value / 12,
+    })
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Unit Converter</h1>
+        <form>
+          <p>
+            {this.state.inches} is {this.state.feet} feet
+          </p>
+          <label>How many inches?</label>
+          <input type="number" onChange={this.inchTracker}></input>
+        </form>
+      </>
+    )
+  }
 }
 
 export default App
